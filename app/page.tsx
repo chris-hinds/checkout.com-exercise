42fc824adcd26e6dab91791dc8b2c5731a9c8dad
Chris Hinds <chris@chrishinds.dev>
2022-11-22 14:03:50 +0000

    feat: include form structure and responsive styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 // Types
 import type { NextPage } from "next";
 
+// Components
+import FeedbackForm from "./components/FeedbackForm";
+
 const FeedbackPage: NextPage = () => {
-  return <div>FeedbackPage</div>;
+  return (
+    <div className="flex items-center h-[calc(100vh-56px)] max-w-full md:max-w-[75%] mx-auto">
+      <FeedbackForm />
+    </div>
+  );
 };
 
 export default FeedbackPage;
